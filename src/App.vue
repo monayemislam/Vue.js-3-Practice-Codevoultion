d0211fbc7102935fef89a3aedf85b1a1043f19b4
DESKTOP-9ORVBHL\Monayem Islam Tamal <monayem.dev@gmail.com>
2023-03-09 10:06:56 +0600

    Vue JS 3 Tutorial - 11 - Conditional Rendering (Part 1)

--- src/App.vue
+++ src/App.vue
@@ -1,70 +1,26 @@
 <template>
-<h2 :id="headingId">Heading</h2>
-<button :disabled="isDisabled">Bind</button>
-<h2 class="underline">underline Text</h2>
-<h2 class="underline" :class="status">Status</h2>
-<h2 :class="isPromoted && 'promoted'">Promoted Movied</h2>
-<h2 :class="isSoldout ? 'sold-out' : 'new' ">Sold Out</h2>
-<h2 :class="['new','promoted']">Newly Promoted</h2>
-<h2 :class="[isPromoted && 'promoted', isSoldout ? 'sold-out' : 'new']">Array Conditional movie</h2>
+  <h2 v-if="num === 0">The number is zero</h2>
+  <h2 v-else-if="num < 0">The number is negative</h2>
+  <h2 v-else-if="num > 0">The number is positive</h2>
+  <h2 v-else>Not A Number</h2>
 
-<h2 :class="{
-  promoted: isPromoted,
-  new: !isSoldout,
-  'sold-out': isSoldout
-}">Object Conditional Movie
-</h2>
-
-<h2 :style="{
-  color: highlightColor,
-  // 'font-size': headerSize + 'px',
-  fontSize: headerSize + 'px',
-  padding: 20,
-}">Inline Style</h2>
-
-<h2 :style="headerStyleObject">Inline Style</h2>
-
-<h2 :style="[baseStyleObject, successStyleObject]">Inline Style</h2>
-
-<h2 :style="[baseStyleObject, dangerStyleObject]">Inline Style</h2>
-
+  <div v-if="display">
+    <h2>Monayem</h2>
+    <h2>Return One</h2>
+    <h2>Vue</h2>
+  </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  data(){
+  name: "App",
+  data() {
     return {
-      headingId: "heading",
-      isDisabled: true,
-      status: 'danger',
-      isPromoted: false,
-      isSoldout: true,
-      headerSize: 50,
-      highlightColor: 'orange',
-      headerStyleObject:{
-        color: 'orange',
-        fontSize: '50px',
-        padding: '20px',
-      },
-      baseStyleObject: {
-        fontSize: '50px',
-        padding: '10px',
-      },
-      successStyleObject: {
-        color: 'green',
-        backgroundColor: 'lightgreen',
-        border: '1px solid green'
-      },
-      dangerStyleObject: {
-        color: 'darkred',
-        backgroundColor: 'red',
-        border: '1px solid darkred'
-      },
-    }
-  }
-}
+      num: "Hi",
+      display: true,
+    };
+  },
+};
 </script>
 
 <style>
@@ -75,18 +31,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-.underline{
-  text-decoration: underline;
-}
-.promoted{
-  font-style: italic;
-}
-.new{
-  color: olivedrab;
-}
-.sold-out{
-  color: #f00;
 }
 </style>
