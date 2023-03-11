2d20b0c60de11635f022399962b9752b9a4aa072
monayemislam <monayemislam@gmail.com>
2023-03-11 12:01:00 +0600

    Vue JS 3 Tutorial - 15 - Conditional List Rendering

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,6 @@
 <template>
-  <h2 v-for="(name, index) in names" :key="name">{{ index }} {{ name }}</h2>
-  <h2 v-for="name in fullNames" :key="name.first">
-    {{ name.first }} {{ name.last }}
-  </h2>
-
-  <div v-for="writter in writters" :key="writter.name">
-    <h2>{{ writter.name }}</h2>
-    <h3 v-for="book in writter.books" :key="book">{{ book }}</h3>
-  </div>
-
-  <h2 v-for="(value, key, index) in myInfo" :key="value">
-    {{ index }} {{ key }} {{ value }}
-  </h2>
-
   <template v-for="name in names" :key="name">
-    <h2>{{ name }}</h2>
-    <hr />
+    <h2 v-if="name=='Nafi'">{{ name }}</h2>
   </template>
 </template>
 
@@ -25,29 +10,9 @@ export default {
   data() {
     return {
       names: ["Monayem", "Nafi", "Nawsin"],
-      fullNames: [
-        { first: "Monayem", last: "Islam" },
-        { first: "Zobayer", last: "Nafi" },
-        { first: "Tasnuva", last: "Nawsin" },
-      ],
-      writters: [
-        {
-          name: "Ahmed Sofa",
-          books: ["Gavi Bittanto", "Bangali Musolmaner Mon"],
-        },
-        {
-          name: "Somoresh Mojumder",
-          books: ["Satkahon", "At kuthuri Noy doroja"],
-        },
-      ],
-      myInfo: {
-        name: "Monayem Islam Tamal",
-        channel: "Return One",
-        Course: "Vue 3",
-      },
-    };
+    }
   },
-};
+}
 </script>
 
 <style>
