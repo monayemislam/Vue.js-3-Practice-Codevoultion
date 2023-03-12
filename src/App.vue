5786a36f0b9e5a4195746524270019f2276eb33c
DESKTOP-9ORVBHL\Monayem Islam Tamal <monayem.dev@gmail.com>
2023-03-12 16:41:29 +0600

    Vue JS 3 Tutoial - 23 - Computed Properties

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <template>
-  <div>
-    <h2 v-once>{{ name }}</h2>
-  </div>
-  <div>
-    <button @click="name = 'Batman'">Change Name</button>
-  </div>
-  <h2 v-pre>{{ name }}</h2>
+  <h2>Full Name - {{ firstName }} {{ lastName }}</h2>
+  <h2>Computed Full Name - {{ fullName }}</h2>
 </template>
 
 <script>
@@ -13,8 +8,14 @@ export default {
   name: "App",
   data() {
     return {
-      name: "Monayem",
+      firstName: "Monayem",
+      lastName: "Islam",
     };
+  },
+  computed: {
+    fullName() {
+      return `${this.firstName} ${this.lastName}`;
+    },
   },
 };
 </script>
@@ -24,32 +25,8 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  /* text-align: center; */
+  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-label {
-  font-weight: bold;
-  display: flex;
-  margin-right: 20px;
-  margin-top: 10px;
-}
-input + label {
-  font-weight: bold;
-  display: inline-flex;
-  margin-right: 20px;
-}
-input[type="text"],
-textarea,
-select {
-  display: block;
-  width: 400px;
-  padding: 6px 12px;
-  font-size: 14px;
-  line-height: 1.43;
-  color: #555;
-  background-color: #fff;
-  margin-top: 5px;
-  background-image: none;
 }
 </style>
