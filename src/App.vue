b7d643a89a14038c50039071e116d77ecde43b1b
DESKTOP-9ORVBHL\Monayem Islam Tamal <monayem.dev@gmail.com>
2023-03-12 15:04:30 +0600

    Vue JS 3 Tutorial - 22 - Bonus Directives

--- src/App.vue
+++ src/App.vue
@@ -1,107 +1,11 @@
 <template>
   <div>
-    <p>{{ JSON.stringify(formValues, null, 2) }}</p>
+    <h2 v-once>{{ name }}</h2>
   </div>
-  <form @submit="submitForm">
-    <div>
-      <label for="name">Name</label>
-      <input type="text" id="name" v-model.trim.lazy="formValues.name" />
-    </div>
-    <div>
-      <label for="profile">Profile Summary</label>
-      <textarea id="profile" v-model="formValues.profileSummary"></textarea>
-    </div>
-    <div>
-      <label for="country">Country</label>
-      <select id="country" v-model="formValues.country">
-        <option value="">Select a Country</option>
-        <option value="Bangladesh">Bangladesh</option>
-        <option value="India">India</option>
-        <option value="Pakistan">Pakistan</option>
-        <option value="Singapore">Singapore</option>
-      </select>
-    </div>
-    <div>
-      <label for="job-location">Job Location</label>
-      <select id="job-location" multiple v-model="formValues.jobLocation">
-        <option value="Bangladesh">Bangladesh</option>
-        <option value="India">India</option>
-        <option value="Pakistan">Pakistan</option>
-        <option value="Singapore">Singapore</option>
-      </select>
-    </div>
-    <div>
-      <input
-        type="checkbox"
-        id="remoteWork"
-        v-model="formValues.remoteWork"
-        true-value="yes"
-        false-value="no"
-      />
-      <label for="remoteWork">Open to remote Work?</label>
-    </div>
-    <div>
-      <label>Skill Set</label>
-      <input
-        type="checkbox"
-        value="html"
-        id="html"
-        v-model="formValues.skillSet"
-      />
-      <label for="html">HTML</label>
-      <input
-        type="checkbox"
-        value="CSS"
-        id="css"
-        v-model="formValues.skillSet"
-      />
-      <label for="css">CSS</label>
-      <input
-        type="checkbox"
-        value="JavaScript"
-        id="javascript"
-        v-model="formValues.skillSet"
-      />
-      <label for="javascript">JavaScript</label>
-    </div>
-    <div>
-      <label>Years of Experience</label>
-      <input
-        type="radio"
-        value="0-2"
-        id="0-2"
-        v-model="formValues.yearsOfExperience"
-      />
-      <label for="0-2">0-2</label>
-      <input
-        type="radio"
-        value="3-5"
-        id="3-5"
-        v-model="formValues.yearsOfExperience"
-      />
-      <label for="3-5">3-5</label>
-      <input
-        type="radio"
-        value="6-10"
-        id="6-10"
-        v-model="formValues.yearsOfExperience"
-      />
-      <label for="6-10">6-10</label>
-    </div>
-
-    <div>
-      <label for="age">Age</label>
-      <input
-        @keyup.enter="submitForm"
-        type="number"
-        id="age"
-        v-model.number="formValues.age"
-      />
-    </div>
-    <!-- <div>
-      <button>Submit</button>
-    </div> -->
-  </form>
+  <div>
+    <button @click="name = 'Batman'">Change Name</button>
+  </div>
+  <h2 v-pre>{{ name }}</h2>
 </template>
 
 <script>
@@ -109,23 +13,8 @@ export default {
   name: "App",
   data() {
     return {
-      formValues: {
-        name: "",
-        profileSummary: "",
-        country: "",
-        jobLocation: [],
-        remoteWork: false,
-        skillSet: [],
-        yearsOfExperience: "",
-        age: null,
-      },
+      name: "Monayem",
     };
-  },
-  methods: {
-    submitForm(event) {
-      event.preventDefault();
-      console.log("Form Values", this.formValues);
-    },
   },
 };
 </script>
