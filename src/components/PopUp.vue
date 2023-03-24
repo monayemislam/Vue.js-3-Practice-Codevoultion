63b6ebcdbf533b98d84a0d7df5d77d005186ef50
monayemislam <monayemislam@gmail.com>
2023-03-24 15:38:58 +0600

    Vue JS 3 Tutorial - 35 - Validating Emitted Events

--- src/components/PopUp.vue
+++ src/components/PopUp.vue
@@ -1,14 +1,29 @@
 <template>
   <div>
     <h2>Pop up message</h2>
-    <button @click="$emit('close','MOnayem')">Hide</button>
+    <input type="text" v-model="name">
+    <button @click="$emit('close',name)">Hide</button>
   </div>
 </template>
 
 <script>
 export default {
     name: 'PopUp',
-    emits: ['close']
+    emits: {
+      close: (name)=>{
+        if(!name){
+          return false;
+        }
+        else{
+          return true;
+        }
+      }
+    },
+    data(){
+      return{
+        name: ''
+      }
+    }
 }
 </script>
 
